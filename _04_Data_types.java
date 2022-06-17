c8d393375f4323178409852cc122906dce8e873b
V.Divya Sree <vakkalagaddadivyasree@gmail.com>
2022-06-17 12:49:13 +0530

    Java_Full_Stack

---
+++ _04_Data_types.java
@@ -0,0 +1,15 @@
+
+public class _04_Data_types {
+  public static void main(String[] args) {
+    short s = 654;
+    byte by = 32;
+    int i = 32;
+    boolean b = true;
+    char ch = 'A';
+    float f = 4.5f;
+    double d = 5.83;   // 5.83d is also valid and it is the default floating value 
+    String str ="Hey Divya";
+    System.out.println(s + "\n" + by + "\n" + i + "\n" + b + "\n" + ch + "\n" + f + "\n" + d + "\n" + str);
+  
+  }
+}
---
+++ _05_variables.java
@@ -0,0 +1,9 @@
+public class _05_variables {
+  static int i = 54;  //global variable
+  public static void main(String[] args) {
+
+    int  i = 43;  //local variable
+    //local variable overrides the global variable 
+    System.out.println(i);
+  }
+}
